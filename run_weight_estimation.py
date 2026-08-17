035869c783a81e0978007b0e86c315c08667d162
Michal Novomestsky <110141821+Michal-Novomestsky@users.noreply.github.com>
2026-08-17 15:15:04 +1000

    Add basic weight calculation pipeline (#1)
    First pass at weight estimation

---
+++ run_weight_estimation.py
@@ -0,0 +1,63 @@
+from tools.constants import FT2M
+from tools.weight_calc import get_mtow, get_w_payload, get_w_ij_climb, get_w_ij_cruise, get_w_ij_descent, get_w_ij_landing, get_w_ij_loiter, get_w_ij_warmup_takeoff
+from tools.constants import *
+
+import warnings
+
+c_t = 14e-6 # kg/Ns Estimate from slides
+M = 0.85
+L_D = 20 # Approximation from slides
+W_EMPTY_0_INIT = 0.5  # initial empty-weight fraction guess
+MAX_ITERS = 10
+
+W_PAYLOAD = 57493  # Estimate from Michal's W1 work
+SAFETY_FACTOR_FUEL = 0.05 # 5% trip fuel contingency
+R = 8000e3 # 8000 km
+E_LOITER = 30*60 # 30 min
+LOITER_ALT = 1500 * FT2M
+warnings.warn("Loiter altitude specified in RFP, but not used in endurance eqn", UserWarning)
+CRUISE_ALT = 35000 * FT2M # NOTE SERVICE CEILING NOT CRUISING ALT - service ceiling should yield max MTOW afaik anyway
+
+
+
+def _print_mtow_chain(mtow_chain, decimals=3):
+    headers = list(mtow_chain.keys())
+    rows = zip(*(mtow_chain[h] for h in headers))
+    fmt = f".{decimals}f"
+
+    col_widths = [len(h) for h in headers]
+    formatted = []
+    for row in rows:
+        cells = tuple(f"{float(v):{fmt}}" for v in row)
+        formatted.append(cells)
+        col_widths = [max(w, len(c)) for w, c in zip(col_widths, cells)]
+
+    header_line = "  ".join(h.ljust(w) for h, w in zip(headers, col_widths))
+    sep_line = "  ".join("-" * w for w in col_widths)
+    print(header_line)
+    print(sep_line)
+    for cells in formatted:
+        print("  ".join(c.ljust(w) for c, w in zip(cells, col_widths)))
+
+
+if __name__ == "__main__":
+    # Flight profile
+    w_profile = [
+        get_w_ij_warmup_takeoff(),
+        get_w_ij_climb(),
+        get_w_ij_cruise(R, c_t, M, L_D, CRUISE_ALT),
+        get_w_ij_loiter(E_LOITER, c_t, L_D),
+        get_w_ij_descent(),
+        get_w_ij_landing(),
+    ]  
+
+    mtow_chain = get_mtow(
+        W_PAYLOAD,
+        w_profile,
+        W_EMPTY_0_INIT,
+        SAFETY_FACTOR_FUEL,
+        max_iters=MAX_ITERS,
+    )
+    _print_mtow_chain(mtow_chain)
+
+    print(f"Weight fractions: {w_profile}")
---
+++ tools/weight_calc.py
@@ -0,0 +1,82 @@
+import math
+import warnings
+
+from tools.isa import get_a
+from tools.constants import *
+
+# TODO find the right name for this (w_payload + w_crew) - what is it? It's not OEW
+def get_w_payload(safety_factor):
+    raise NotImplementedError
+
+# TODO Should this just be a const instead?
+def get_w_ij_warmup_takeoff():
+    return 0.970
+
+def get_w_ij_climb():
+    return 0.985
+
+def get_w_ij_descent():
+    warnings.warn("Assuming descent fuel consumption equal to climb", UserWarning)
+    return get_w_ij_climb()
+
+def get_w_ij_landing():
+    return 0.995
+
+def get_w_ij_cruise(R, c_t, M, L_D, z):
+    a = get_a(z)
+    V = M * a
+
+    warnings.warn("Assuming plane cruising at 0.866(L/D)", UserWarning)
+    L_D *= 0.866
+
+    return math.exp(-R * G_GRAV * c_t / (V * L_D))
+
+def get_w_ij_loiter(E, c_t, L_D):
+    return math.exp(-E * G_GRAV * c_t /L_D)
+
+# TODO better way to do this?
+def get_w_fuel_0(w_profile, safety_factor):
+    w_start_end = 1
+    for w_i_j in w_profile:
+        w_start_end *= w_i_j
+
+    return (1 + safety_factor) * (1 - w_start_end)
+
+def get_w_empty_0(mtow):
+    # TODO add other correlations, not just Raymer
+    A = 0.97
+    C = -0.06
+    return A*mtow**C
+
+
+def mtow_iter(w_payload, w_fuel_0, w_empty_0):
+    return w_payload/(1 - w_fuel_0 - w_empty_0)
+
+def get_mtow(w_payload, w_profile, w_empty_0_init, safety_factor, max_iters=10):
+    mtow_prev = 0
+    w_empty_0 = w_empty_0_init
+    mtow_chain = {
+        "i": [],
+        "mtow": [],
+        "w_empty_0": [],
+        "w_fuel_0": [],
+        "eps": [],
+    }
+
+    w_fuel_0 = get_w_fuel_0(w_profile, safety_factor)
+    
+    # Iteratively solve
+    for i in range(max_iters):
+        mtow = mtow_iter(w_payload, w_fuel_0, w_empty_0)
+        eps = abs((mtow - mtow_prev)/mtow)
+
+        w_empty_0 = get_w_empty_0(mtow)
+        mtow_prev = mtow
+
+        mtow_chain["i"].append(i)
+        mtow_chain["mtow"].append(mtow)
+        mtow_chain["w_empty_0"].append(w_empty_0)
+        mtow_chain["w_fuel_0"].append(w_fuel_0)
+        mtow_chain["eps"].append(eps)
+
+    return mtow_chain
